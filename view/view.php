5ae4d0b7804b7939b6ed4360fb8326731e2b3539
Jhampoll Naranjo <jhampoll.naranjo@atlanticsoft.us>
2016-01-15 13:52:30 -0500

    se le  agrego la clase responsive

--- view/view.php
+++ view/view.php
@@ -12,28 +12,30 @@
      ?>
     <h1 class="text-center">Listado de Users</h1>
     <div class="container">
-        <div class="row">
-            <table  class="table">
-                <thead>
-                    <tr>
-                        <th>name</th>
-                        <th>last name</th>
-                        <th>Age</th>
-                    </tr>
-                </thead>
-                
-                <?php foreach($datos as $row): ?>
-                <tbody>
-                    <tr>
-                        <td><?php echo $row['name']?></td>
-                        <td><?php echo $row['last_name']?></td>
-                        <td><?php echo $row['edad']?></td>
-                        <td><a href="../controller/controller.php?id=<?php echo $row['id']?>">Eliminar</a></td>
-                    </tr>
-                </tbody>
-                <?php endforeach;?>
-            </table>
-            <a href="adduser.php"  class="btn btn-primary btn-sm">agregar</a>
+        <div class="col-md-6">
+            <div class="row table-responsive">
+                <table  class="table">
+                    <thead>
+                        <tr>
+                            <th>name</th>
+                            <th>last name</th>
+                            <th>Age</th>
+                        </tr>
+                    </thead>
+                    
+                    <?php foreach($datos as $row): ?>
+                    <tbody>
+                        <tr>
+                            <td><?php echo $row['name']?></td>
+                            <td><?php echo $row['last_name']?></td>
+                            <td><?php echo $row['edad']?></td>
+                            <td><a href="../controller/controller.php?id=<?php echo $row['id']?>">Eliminar</a></td>
+                        </tr>
+                    </tbody>
+                    <?php endforeach;?>
+                </table>
+                <a href="adduser.php"  class="btn btn-primary btn-sm">agregar</a>
+            </div>
         </div>
     </div>
 </body>
